904f5f5930bf462aee053209ac9f1acbf32fd51a
okotovenko <oakotovenko@ukr.net>
2022-08-31 16:14:14 +0300

    Changes settings for mobile (100vh don't work)

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,3 +1,8 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const screenWidth = window.screen.width;
 
 let headerIconsBottom = document.querySelector('.header__icons-bottom > a');
@@ -6,6 +11,12 @@ headerIconsBottom.addEventListener('click', function () {
 		headerContainer.classList.remove('active');
 	}
 });
+
+let home = document.querySelector('#home');
+home.addEventListener('click', function () {
+	short.classList.remove('active');
+})
+
 
 let short = document.querySelector('.short');
 let navBurger = document.querySelector('.nav__burger');
